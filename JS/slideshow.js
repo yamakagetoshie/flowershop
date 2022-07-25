28c6961ad71fbc6793b1da5b3e5c5fe1ceb5763a
yamakagetoshie <97348551+yamakagetoshie@users.noreply.github.com>
2022-07-25 16:58:35 +0900

    Add files via upload

--- JS/slideshow.js
+++ JS/slideshow.js
@@ -19,10 +19,10 @@ var windowwidth = window.innerWidth || document.documentElement.clientWidth || 0
 
 $('#slider').vegas({
     overlay: false,//画像の上に網線やドットのオーバーレイパターン画像を指定。
-    transition: 'blur',//切り替わりのアニメーション。
+    transition: 'blur',//切り替わりのアニメーション。http://vegas.jaysalvat.com/documentation/transitions/参照。fade、fade2、slideLeft、slideLeft2、slideRight、slideRight2、slideUp、slideUp2、slideDown、slideDown2、zoomIn、zoomIn2、zoomOut、zoomOut2、swirlLeft、swirlLeft2、swirlRight、swirlRight2、burnburn2、blurblur2、flash、flash2が設定可能。
     transitionDuration: 2000,//切り替わりのアニメーション時間をミリ秒単位で設定
     delay: 10000,//スライド間の遅延をミリ秒単位で。
     animationDuration: 20000,//スライドアニメーション時間をミリ秒単位で設定
-    animation: 'kenburns',//スライドアニメーションの種類。
+    animation: 'kenburns',//スライドアニメーションの種類。http://vegas.jaysalvat.com/documentation/transitions/参照。kenburns、kenburnsUp、kenburnsDown、kenburnsRight、kenburnsLeft、kenburnsUpLeft、kenburnsUpRight、kenburnsDownLeft、kenburnsDownRight、randomが設定可能。
     slides: responsiveImage,//画像設定を読む
   });
